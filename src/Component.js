b59086330ee5d666c6a36d9ac0a9a0439887b70d
ecruz182 <edcruz182@gmail.com>
2024-05-17 19:21:45 -0500

    Add event onChange and onClick

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Component(){
+
+    const [text, setText] = useState()
+    const [update, setUpdate] = useState()
+
+    const textOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const buttonOnClick = () => {
+        setUpdate(text)
+    }
+
+
     return(
-        <div>My Component</div>
+        <div>
+            <input type="text" value={text} onChange={textOnChange}/>
+            <button onClick={buttonOnClick}>Actualizar</button>
+            <p>Texto input: {text}</p>
+            <p>texto Actualizado: {update} </p>
+        </div>
     )
 }
